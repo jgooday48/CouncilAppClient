41925e5af3abc89eb3eb372c29bb15f72f761c81
jarvis <jsd.ditcher@outlook.com>
2023-11-27 17:01:01 +0000

    fix MyAccount refreshing bug, add create/loadInitiative

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,44 @@
-// let loginBtn = document.getElementById('openPopUp')
-// let registerBtn = document.getElementById('openRegister')
-// let exitBtn = document.getElementById('exitBtn')
-
-document.getElementById('openPopUp').addEventListener('click', () => {
+document.getElementById('openPopUp').addEventListener('click', (e) => {
+    e.preventDefault()
     document.querySelector('.register').style.display = 'none'
     document.querySelector('.login').style.display = 'flex'
 })
 
-document.getElementById('exitBtn').addEventListener('click', () => {
+document.getElementById('exitBtn').addEventListener('click', (e) => {
+    e.preventDefault()
     document.querySelector('.login').style.display = 'none'
 })
-document.getElementById('exitBtn2').addEventListener('click', () => {
+document.getElementById('exitBtn2').addEventListener('click', (e) => {
+    e.preventDefault()
     document.querySelector('.register').style.display = 'none'
     document.querySelector('.login').style.display = 'flex'
 })
 
-document.getElementById('openRegister').addEventListener('click', () => {
+document.getElementById('openRegister').addEventListener('click', (e) => {
+    e.preventDefault()
     document.querySelector('.login').style.display = 'none'
     document.querySelector('.register').style.display = 'flex'
 })
 
+function createInitiative(){
+    const initiative = document.createElement('div');
+    initiative.className ='initiative'
+    const title = document.createElement("h2")
+    title.textContent = 'U-Turn'
+    const text = document.createElement("p")
+    text.textContent = 'We are stopping recyycling'
+
+    initiative.appendChild(title)
+    initiative.appendChild(text)
+    return initiative
+}
+
+async function loadInitiatives(){
+    const container = document.getElementById('initiatives')
+
+    const ini = createInitiative()
+    container.appendChild(ini)
+}
+
+
+loadInitiatives()
