4a50ed70bf26169f5bbcc1a70a3538fc54bd98ec
jgooday48 <joelgooday48@gmail.com>
2023-11-29 12:39:26 +0000

    merge test

--- assets/js/login.js
+++ assets/js/login.js
@@ -23,7 +23,7 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
         localStorage.setItem("token", data.token)
         localStorage.setItem("user_id", data.user_id)
         console.log(data.user_id)
-        window.alert('You are logged in')
+        // window.alert('You are logged in')
         window.location.assign("index.html")
     } else {
         alert(data.error);
